d0aa97f5b66825d4cabc7fc02be0c75a361e7da9
Uzair Ashraf <uzinatorcl@live.com>
2021-01-04 14:41:41 -0800

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 3000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              "@babel/preset-typescript"
+            ]
+          }
+        },
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'source-map'
+};
